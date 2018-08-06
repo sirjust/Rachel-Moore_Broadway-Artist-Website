b96660f6fce873409c3b2ad1f6677b03d7a29305
SirJUST <theoperaking@yahoo.com>
2018-08-06 00:18:45 -0700

    fix fade and start modal

--- js/RachelScript.js
+++ js/RachelScript.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-$("body").css("display", "none");
-$("body").fadeIn(1000);
-$("a.page-transition").click(function(event){
-	event.preventDefault();
-	linkLocation = this.href;
-	$("body").fadeOut(700, redirectPage);
+$(function() {
+$('body').removeClass('fade-in');
 });
-
-function redirectPage() {
-	window.location = linkLocation;
-	}
-});
-
